47cb0e2c302b17875a3a325f273c482905630888
David Horwitz <dhorwitz@gmail.com>
2008-04-21 16:34:59 +0000

    Main view done (bar image) + link to edit

---
+++ tool/src/java/org/sakaiproject/profilewow/tool/producers/EditProducer.java
@@ -0,0 +1,22 @@
+package org.sakaiproject.profilewow.tool.producers;
+
+import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.view.ComponentChecker;
+import uk.org.ponder.rsf.view.ViewComponentProducer;
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+
+public class EditProducer implements ViewComponentProducer {
+
+	public static final String VIEW_ID = "edit";
+	public String getViewID() {
+		// TODO Auto-generated method stub
+		return VIEW_ID;
+	}
+
+	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
+			ComponentChecker checker) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
