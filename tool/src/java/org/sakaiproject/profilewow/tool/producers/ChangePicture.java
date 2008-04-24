c96f61856184b601270c94c13a57999161429f35
David Horwitz <dhorwitz@gmail.com>
2008-04-24 13:27:39 +0000

    Now fetches profile pics from that folder

--- tool/src/java/org/sakaiproject/profilewow/tool/producers/ChangePicture.java
+++ tool/src/java/org/sakaiproject/profilewow/tool/producers/ChangePicture.java
@@ -1,22 +1,192 @@
 package org.sakaiproject.profilewow.tool.producers;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.content.api.ContentCollection;
+import org.sakaiproject.content.api.ContentCollectionEdit;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.content.api.ContentResource;
+import org.sakaiproject.entity.api.Entity;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.entity.api.ResourcePropertiesEdit;
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.TypeException;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.user.api.UserDirectoryService;
+
+import uk.org.ponder.rsf.components.UIBranchContainer;
 import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UILink;
 import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 public class ChangePicture implements ViewComponentProducer {
 
+	private static Log log = LogFactory.getLog(ChangePicture.class);
+	
 	public static final String VIEW_ID="changepic";
+	
+	public static final String COLLECTION_PROFILE = "Profile";
+	
+	/** This string used for Title of the profile collection **/
+	public static final String COLLECTION_PROFILE_TITLE = "Profile Data";
+	/** This string gives description for profile folder **/
+	public static final String COLLECTION_PROFILE_DESCRIPTION = "Data related to the profile";
+	
 	public String getViewID() {
 		// TODO Auto-generated method stub
 		return VIEW_ID;
 	}
 
+	
+	private ContentHostingService contentHostingService;
+	public void setContentHostingService(ContentHostingService contentHostingService) {
+		this.contentHostingService = contentHostingService;
+	}
+
+	private SiteService siteService;
+
+	public void setSiteService(SiteService siteService) {
+		this.siteService = siteService;
+	}
+	
+	private UserDirectoryService userDirectoryService;
+	public void setUserDirectoryService(UserDirectoryService uds) {
+		this.userDirectoryService = uds;
+	}
+
+	
 	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
 			ComponentChecker checker) {
 		// TODO Auto-generated method stub
+		try {
+			
+			ContentCollection pCollection = contentHostingService.getCollection(retrieveProfileFolderId(getCurrentUserSiteId()));
+			log.info("got a collection with " + pCollection.getMemberCount() + " objects");
+			
+			List resources = pCollection.getMemberResources();
+			for (int i = 0; i < resources.size(); i++) {
+				UIBranchContainer row = UIBranchContainer.make(tofill, "pic-row:");
+				for (int q =0; q < 4 && i< resources.size(); q++) {
+					ContentResource resource = (ContentResource)resources.get(i);
+					UIBranchContainer cell = UIBranchContainer.make(row, "pic-cell:");
+					UILink.make(cell, "pic", resource.getUrl());
+					
+					i++;
+				}
+				
+				
+			}
+			
+			
+		} catch (PermissionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IdUnusedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TypeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 
 	}
 
+	
+	private String getCurrentUserSiteId() {
+		return siteService.getUserSiteId(userDirectoryService.getCurrentUser().getId());
+	}
+	
+	
+	
+	/**
+	 * Returns profile folder id using 'Profile'. If it
+	 * does not exist will create it.
+	 * 
+	 * @param siteId
+	 *            	The site to search
+	 *            
+	 * @return String 
+	 * 				Contains the complete id for the profile folder
+	 * @throws PermissionException 
+	 * 
+	 * @throws PermissionException
+	 *             Access denied or Not found so not available
+	 */
+	private String retrieveProfileFolderId(String siteId) throws PermissionException {
+
+		final String siteCollection = contentHostingService.getSiteCollection(siteId);
+		String profileCollection = siteCollection + COLLECTION_PROFILE + Entity.SEPARATOR;
+
+		try {
+			contentHostingService.checkCollection(profileCollection);
+			return profileCollection;
+
+		} 
+		catch (IdUnusedException e) {
+			// Does not exist, so try to create it
+			profileCollection = siteCollection + COLLECTION_PROFILE + Entity.SEPARATOR;
+	
+			createProfileFolder(profileCollection, siteId);
+			return profileCollection;
+			
+		} 
+		catch (TypeException e) {
+			log.error("TypeException while getting profile folder using 'Profile' string: "
+							+ e.getMessage(), e);
+			throw new Error(e);
+		}
+
+	}
+	
+	
+	/**
+	 * Creates the profile folder in Resources
+	 * 
+	 * @param profileCollection
+	 * 				The id to be used for the profile folder
+	 * 
+	 * @param siteId
+	 * 				The site id for whom the folder is to be created
+	 */
+	private void createProfileFolder(String profileCollection, String siteId) {
+		try {
+			log.debug("createProfileFolder()");
+			log.info("Could not find profile folder, attempting to create.");
+
+			ContentCollectionEdit collection = 
+						contentHostingService.addCollection(profileCollection);
+			
+			final ResourcePropertiesEdit resourceProperties = collection.getPropertiesEdit();
+			
+			resourceProperties.addProperty(ResourceProperties.PROP_DISPLAY_NAME,
+											COLLECTION_PROFILE_TITLE);
+
+			resourceProperties.addProperty(ResourceProperties.PROP_DESCRIPTION,
+											COLLECTION_PROFILE_DESCRIPTION);
+
+			contentHostingService.commitCollection(collection);
+
+			contentHostingService.setPubView(collection.getId(), true);
+		} 
+		catch (Exception e) {
+			// catches	IdUnusedException, 		TypeException
+			//			InconsistentException,	IdUsedException
+			//			IdInvalidException		PermissionException
+			//			InUseException
+			log.error(e.getMessage() + " while attempting to create Profile folder: "
+							+ " for site: " + siteId + ". NOT CREATED... " + e.getMessage(), e);
+			throw new Error(e);
+		}
+	}
+
+
+	
+	
 }
--- tool/src/java/org/sakaiproject/profilewow/tool/producers/EditProducer.java
+++ tool/src/java/org/sakaiproject/profilewow/tool/producers/EditProducer.java
@@ -12,6 +12,7 @@ import org.sakaiproject.user.api.User;
 import org.sakaiproject.user.api.UserDirectoryService;
 
 import uk.org.ponder.messageutil.MessageLocator;
+import uk.org.ponder.messageutil.TargettedMessageList;
 import uk.org.ponder.rsf.components.UIBranchContainer;
 import uk.org.ponder.rsf.components.UICommand;
 import uk.org.ponder.rsf.components.UIContainer;
@@ -33,7 +34,7 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 public class EditProducer implements ViewComponentProducer, DefaultView {
 
 	public static final String VIEW_ID = "edit";
-	
+
 	private static Log log = LogFactory.getLog(EditProducer.class);
 	public String getViewID() {
 		// TODO Auto-generated method stub
@@ -44,35 +45,54 @@ public class EditProducer implements ViewComponentProducer, DefaultView {
 	public void setSakaiPersonManager(SakaiPersonManager in) {
 		spm = in;
 	}
-	
+
 	private MessageLocator messageLocator;
 	public void setMessageLocator(MessageLocator messageLocator) {
 
 		this.messageLocator = messageLocator;
 	}
 
-	
+
 	private UserDirectoryService userDirectoryService;
 	public void setUserDirectoryService(UserDirectoryService uds) {
 		this.userDirectoryService = uds;
 	}
-	
+
+
+	private TargettedMessageList tml;
+	public void setTargettedMessageList(TargettedMessageList tml) {
+		this.tml = tml;
+	}
+
 	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
 			ComponentChecker checker) {
-		// TODO Auto-generated method stub
-		
+
+		//process any messages
+		if (tml.size() > 0) {
+			for (int i = 0; i < tml.size(); i ++ ) {
+				UIBranchContainer errorRow = UIBranchContainer.make(tofill,"error-row:", new Integer(i).toString());
+				if (tml.messageAt(i).args != null ) {	    		
+					UIMessage.make(errorRow,"error",tml.messageAt(i).acquireMessageCode(),(String[])tml.messageAt(i).args[0]);
+				} else {
+					UIMessage.make(errorRow,"error",tml.messageAt(i).acquireMessageCode());
+				}
+
+			}
+		}
+
+
 		UIForm form = UIForm.make(tofill,"edit-form");
-		
+
 		SakaiPerson sPerson = spm.getSakaiPerson(spm.getUserMutableType());
 		if (sPerson == null) {
 			sPerson = spm.create(userDirectoryService.getCurrentUser().getId(), spm.getUserMutableType());
 			spm.save(sPerson);
 		}
-		
+
 		log.info("got profile for: " + sPerson.getGivenName() + " " + sPerson.getSurname());
-		
+
 		String otpBean = "profileBeanLocator." + sPerson.getUid() + ".sakaiPerson";
-		
+
 		UIInput.make(form,"editProfileForm-first_name", otpBean + ".givenName" ,sPerson.getGivenName());
 		UIInput.make(form,"editProfileForm-lname", otpBean + ".surname", sPerson.getSurname());
 		UIInput.make(form,"editProfileForm-nickname", otpBean + ".nickname", sPerson.getNickname());
@@ -84,114 +104,114 @@ public class EditProducer implements ViewComponentProducer, DefaultView {
 		UIInput.make(form,"editProfileForm-title", otpBean + ".title", sPerson.getTitle());
 		//not in profile data yet
 		UIInput.make(form,"editProfileForm-country", otpBean + ".localityName", sPerson.getLocalityName());
-		
+
 		UIInput.make(form,"editProfileForm-homepage", otpBean + ".labeledURI", sPerson.getLabeledURI());
 		UIInput.make(form,"editProfileForm-workphone", otpBean + ".telephoneNumber", sPerson.getTelephoneNumber());
 		UIInput.make(form,"editProfileForm-mobile", otpBean + ".mobile", sPerson.getMobile());
 		UIInput.make(form,"editProfileForm-more", otpBean + ".notes", sPerson.getNotes());
-		
+
 		//picture stuff
 		String picUrl = sPerson.getPictureUrl();
 		if (picUrl == null || picUrl.trim().length() == 0)
 			picUrl = "../images/pictureUnavailable.jpg";
 		else 
 			picUrl = sPerson.getPictureUrl();
-		
-		
+
+
 		if (sPerson.isSystemPicturePreferred() != null &&  sPerson.isSystemPicturePreferred().booleanValue()) {
 			UIInternalLink.make(form, "current-pic", new ImageViewParamaters("imageServlet", sPerson.getUuid()));
 		} else if (sPerson.isSystemPicturePreferred() == null) {
 			UILink.make(form, "current-pic", picUrl);
 		} 
-		
+
 		//hide
 		String hideS = "false";
 		if (sPerson.getHidePrivateInfo()!=null && sPerson.getHidePrivateInfo().booleanValue()) {
 			hideS = "true";
 		}
 		log.info("hide personal is " + hideS);
-		
+
 		UISelect hide = UISelect.make(form, "hide-select",new String[]{"true", "false"},
 				new String[]{messageLocator.getMessage("editProfile.sms.yes"), messageLocator.getMessage("editProfile.sms.no")}, 
 				otpBean + ".hidePrivateInfo", hideS);
-		
-		 String hideID = hide.getFullID();
-	     
-		 for(int i = 0; i < 2; i ++ ) {
-			 UIBranchContainer radiobranch = UIBranchContainer.make(form,
-					 "hideSelect:", new Integer(i).toString());
-			 UISelectChoice choice = UISelectChoice.make(radiobranch, "editProfile-hide", hideID, i);
-			 UISelectLabel lb = UISelectLabel.make(radiobranch, "hide-label", hideID, i);
-			 UILabelTargetDecorator.targetLabel(lb, choice);
-		 }
-		
-		
+
+		String hideID = hide.getFullID();
+
+		for(int i = 0; i < 2; i ++ ) {
+			UIBranchContainer radiobranch = UIBranchContainer.make(form,
+					"hideSelect:", new Integer(i).toString());
+			UISelectChoice choice = UISelectChoice.make(radiobranch, "editProfile-hide", hideID, i);
+			UISelectLabel lb = UISelectLabel.make(radiobranch, "hide-label", hideID, i);
+			UILabelTargetDecorator.targetLabel(lb, choice);
+		}
+
+
 		//sms preference
-		 
+
 		UISelect sms = UISelect.make(form, "sms-select",new String[]{"true", "false"},
 				new String[]{messageLocator.getMessage("editProfile.sms.yes"), messageLocator.getMessage("editProfile.sms.no")}, 
 				"profileBeanLocator." + sPerson.getUid() + ".smsNotifications", recieveSMSNotifications().toString());
-		 String selectID = sms.getFullID();
-	     
-		 for(int i = 0; i < 2; i ++ ) {
-			 UIBranchContainer radiobranch = UIBranchContainer.make(form,
-					 "smsSelect:", new Integer(i).toString());
-			 UISelectChoice choice = UISelectChoice.make(radiobranch, "editProfileForm-sms", selectID, i);
-			 UISelectLabel lb = UISelectLabel.make(radiobranch, "smslabel", selectID, i);
-			 UILabelTargetDecorator.targetLabel(lb, choice);
-		 }
+		String selectID = sms.getFullID();
+
+		for(int i = 0; i < 2; i ++ ) {
+			UIBranchContainer radiobranch = UIBranchContainer.make(form,
+					"smsSelect:", new Integer(i).toString());
+			UISelectChoice choice = UISelectChoice.make(radiobranch, "editProfileForm-sms", selectID, i);
+			UISelectLabel lb = UISelectLabel.make(radiobranch, "smslabel", selectID, i);
+			UILabelTargetDecorator.targetLabel(lb, choice);
+		}
 		//UIInput sms = UIInput.make(form, "editProfileForm-sms", "profileBeanLocator." + sPerson.getUid() + ".smsNotifications", "true");
-		
+
 		//UIMessage.make(form, "smslabel","editProfile.sms");
-		
-		
-		
+
+
+
 		UICommand.make(form, "profile-save","save","profileBeanLocator.saveAll");
-		
-		
-		
-		
+
+
+
+
 		UIInternalLink.make(form, "change-pic", messageLocator.getMessage("editProfile.changePic"), new SimpleViewParameters(ChangePicture.VIEW_ID));
-		
+
 		//the change password form
 		UIForm passForm = UIForm.make(tofill, "passForm:");
 		UIInput.make(passForm,"pass1","userBeanLocator." + sPerson.getUid() + ".pass1");
 		UIInput.make(passForm,"pass2","userBeanLocator." + sPerson.getUid() + ".pass2");
 		UICommand.make(passForm, "passSubmit", "userBeanLocator.saveAll");
 	}
-	
-	
+
+
 	/**
 	 * Returns String for image. Uses the config bundle
 	 * to return paths to not available images.  
 	 */
 	private String getUrlOfficialPicture(String userId) {
-		
+
 		String imageUrl = "";
-			
+
 		imageUrl = "ProfileImageServlet.prf?photo=" + userId;
-			
+
 		return imageUrl; 
 	}
-	
-	
+
+
 	private Boolean recieveSMSNotifications() {
 		User u = userDirectoryService.getCurrentUser();
 		ResourceProperties rp = u.getProperties();
-		
+
 		Boolean ret = null;
-		
-			String val = rp.getProperty("smsnotifications");
-			ret = new Boolean(val);
-			log.info("got sms notification of: " + val + ", "+ ret.toString());
-		
+
+		String val = rp.getProperty("smsnotifications");
+		ret = new Boolean(val);
+		log.info("got sms notification of: " + val + ", "+ ret.toString());
+
 		if (ret == null)
 			ret = new Boolean(false);
-		
+
 		return ret;
-		
+
 	}
 
-	
-	
+
+
 }
