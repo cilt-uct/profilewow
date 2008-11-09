608589767af03b9a452b0add91171aa904e23eda
Lovemore Nalube <lovenalube@gmail.com>
2008-11-09 13:18:38 +0000

    VULA-66. Official picture.

--- tool/src/java/org/sakaiproject/profilewow/tool/producers/ChangePicture.java
+++ tool/src/java/org/sakaiproject/profilewow/tool/producers/ChangePicture.java
@@ -30,6 +30,7 @@ import uk.org.ponder.rsf.components.UIContainer;
 import uk.org.ponder.rsf.components.UIForm;
 import uk.org.ponder.rsf.components.UIInternalLink;
 import uk.org.ponder.rsf.components.UILink;
+import uk.org.ponder.rsf.components.UIMessage;
 import uk.org.ponder.rsf.components.UIOutput;
 import uk.org.ponder.rsf.components.UISelect;
 import uk.org.ponder.rsf.components.UISelectChoice;
@@ -75,7 +76,7 @@ public class ChangePicture implements ViewComponentProducer, NavigationCaseRepor
 		// TODO Auto-generated method stub
 		ContentCollection pCollection = resourceUtil.getUserCollection();
 			log.debug("got a collection with " + pCollection.getMemberCount() + " objects");
-			UIForm form = UIForm.make(tofill,"form");
+			
 		
 		SakaiPerson sPerson = spm.getSakaiPerson(spm.getUserMutableType());
 		
@@ -86,7 +87,23 @@ public class ChangePicture implements ViewComponentProducer, NavigationCaseRepor
 		}
 		else
 			UILink.make(tofill, "default-image", NO_PIC_URL);
-			
+		
+		//should only display if there is an official pic
+		if (hasProfilePic()) {
+		// UIInternalLink.make(tofill, "useOf", new SimpleViewParameters(SelectOfficialPictureProducer.VIEW_ID));	
+			UIBranchContainer op = UIBranchContainer.make(tofill, "official-pic:");
+			UIMessage.make(op, "official-pic-title", "official.picture.title");
+			UIInternalLink.make(op, "official-pic-image", new ImageViewParamaters("imageServlet", sPerson.getUuid() ));
+			UIForm form = UIForm.make(op, "official-pic-form");
+			UICommand.make(form, "official-pic-field", UIMessage.make("useOfficialSub"),"uploadBean.useOfficial");
+
+		}
+		
+		UIForm formUpload = UIForm.make(tofill, "upload-pic-form");
+		//UIInput.make(form,"file-upload", "uploadBean")
+		UICommand.make(formUpload,"submit","uploadBean.processUpload");
+
+		UIForm form = UIForm.make(tofill,"form");
 			List<ContentResource> resources = pCollection.getMemberResources();
 			
 			UISelect selectPic = UISelect.makeMultiple(form, "select-pic",
@@ -108,6 +125,18 @@ public class ChangePicture implements ViewComponentProducer, NavigationCaseRepor
 			UICommand.make(form, "submit","Change picture","uploadBean.changePicture");
 	}
 
+	private boolean hasProfilePic() {
+		
+		SakaiPerson sp = spm.getSakaiPerson(spm.getSystemMutableType());
+		
+		if (sp == null)
+			return false;
+		else if (sp.getJpegPhoto() != null)
+			return true;
+	
+		return false;
+		
+	}
 	public List reportNavigationCases() {
 		List togo = new ArrayList(); // Always navigate back to this view.
 		togo.add(new NavigationCase(null, new SimpleViewParameters(VIEW_ID)));
