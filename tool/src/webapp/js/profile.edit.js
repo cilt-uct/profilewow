2918d8a68c67e7abd168cc42229f19cfd7950647
Lovemore Nalube <lovenalube@gmail.com>
2009-02-02 10:08:22 +0000

    VULA-307. Chained regExp checks.

--- tool/src/webapp/js/profile.edit.js
+++ tool/src/webapp/js/profile.edit.js
@@ -17,7 +17,9 @@ $(document).ready(function() {
              */
 
             jQuery.validator.addMethod("phone", function(phone_number, element) {
-                return this.optional(element) || phone_number.length > 9 && phone_number.match(/^[+]?\d+/);
+                var num = phone_number.split(' ').join('');
+                return this.optional(element) || phone_number.length > 9 &&
+                         ((num.match(/^[0-9]/) || num.match(/^[+]/) || num.match(/^[(]/)) && (num.split('-').join('').split('(').join('').split(')').join('').match(/^[+]?\d+$/)));
             }, "Please enter a valid phone number");
 
 
